b2fc4499c2a0ea2ce89ca988185ac9c7ecc878ce
Majid <pirzadamajid003@gmail.com>
2024-10-22 21:27:50 +0530

    Project AI

--- 21. write a python program to input electricity unit charges and calculate total electricity bill.py
+++ 21. write a python program to input electricity unit charges and calculate total electricity bill.py
@@ -4,16 +4,28 @@
 ##for unit above 250 rs 1.50/units
 ##additional surcharge of 20% is added to the bill
 
-unit = int(input("enter unit :"))
-if unit <= 50:
-    bill = unit * 0.50
-elif unit >= 50  <= 150:
-    bill = unit * 0.75
-elif unit >= 150 <= 250:
-    bill = unit * 1.20
-elif unit > 250:
-    bill = unit * 1.50
-else:
-    bill = unit * 20
+a = int(input("enter units of electricity : "))
 
-print("Total Bill :",bill)
+
+if a <=50:
+  Base_Amount = a*0.50
+  Additional_Surcharge = (Base_Amount*20/100)
+  print("BASE AMOUNT: Rs.",Base_Amount)
+  print("ADDITIONAL SURCHARGE:Rs.",Additional_Surcharge)
+  print("TOTAL ELECTRICITY BILL: Rs.",Base_Amount+Additional_Surcharge)
+elif a >50 and a <=150:
+    Base_Amount = (25)+(a-50)*0.75
+    Additional_Surcharge = (Base_Amount*28/100)
+    print("BASE AMOUNT:RS.",Base_Amount)
+    print("ADDITIONAL SURCHARGE:Rs.",Additional_Surcharge)
+    print("TOTAL ELECTRICITY BILL:Rs.",Base_Amount+Additional_Surcharge)
+elif a >150 and a <=250:
+    Base_Amount = (100)+(a-150)*1.20
+    Additional_Surcharge = (Base_Amount*20/100)
+    print("BASE AMOUNT:Rs.",Base_Amount)
+    print("ADDITIONAL SURCHARGE:Rs.",Additional_Surcharge)
+    print("TOTAL ELECTRICITY BILL:Rs",Base_Amount+Additional_Surcharge)
+else:
+    Base_Amount = (220)+(a-250)*1.50
+    Additional_Surcharge = (Base_Amount*20/100)
+    print("TOTAL ELECTRICITY BILL:Rs.",Base_Amount+Additional_Surcharge)
